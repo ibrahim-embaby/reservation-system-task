5dff2c5cdfbfd9e529445388d5fac9c58834ce83
ibrahim-embaby <ibrahimembaby10@gmail.com>
2025-01-08 23:06:31 +0200

    added frontend link to cors

--- server/utils/allowedOrigins.js
+++ server/utils/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://doctor-reservation.netlify.app",
+];
 
 module.exports = allowedOrigins;
